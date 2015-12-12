38d53a2557c1355357ecc3ce9670222359d48317
Spriithy <theo.dano@gmail.com>
2015-12-12 22:27:21 +0100

    MemoryWriter update
    Added Object-oriented (non-static) versions for writing methods, writer
    still needs linking with memory

--- OctoMemory/src/com/projectoctopus/memory/MemoryWriter.java
+++ OctoMemory/src/com/projectoctopus/memory/MemoryWriter.java
@@ -20,11 +20,132 @@
 package com.projectoctopus.memory;
 
 /**
- * @author Théophile Dano, Spriithy 2015
+ * A toolkit for writing a memory's data
+ * @author Theophile Dano, Spriithy 2015
+ * @since 0.1
  */
 public class MemoryWriter {
 
-	private MemoryWriter() {}
+	private Memory	memory;
+	private boolean	linked	= false;
+
+	public void link(Memory memory) {
+		assert(!linked);
+		linked = true;
+		this.memory = memory;
+	}
+
+	public int write_(int ptr, boolean value) {
+		ptr = write_(memory, ptr, (byte) ((value) ? 0x01 : 0x00));
+		return ptr;
+	}
+
+	public int write_(int ptr, boolean[] booleans) {
+		for (boolean value : booleans)
+			ptr = write_(memory, ptr, value);
+		return ptr;
+	}
+
+	public int write_(int ptr, byte value) {
+		memory.alloc_f[ptr] = true;
+		memory.data[ptr++] = value;
+		return ptr;
+	}
+
+	public int write_(int ptr, byte[] bytes) {
+		for (byte value : bytes)
+			ptr = write_(memory, ptr, value);
+		return ptr;
+	}
+
+	public int write_(int ptr, char value) {
+		ptr = write_(memory, ptr, (byte) ((value >> 8) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 0) & 0xff));
+		return ptr;
+	}
+
+	public int write_(int ptr, char[] chars) {
+		for (char value : chars)
+			ptr = write_(memory, ptr, value);
+		return ptr;
+	}
+
+	public int write_(int ptr, double value) {
+		ptr = write_(memory, ptr, Double.doubleToLongBits(value));
+		return ptr;
+	}
+
+	public int write_(int ptr, double[] doubles) {
+		for (double value : doubles)
+			ptr = write_(memory, ptr, Double.doubleToLongBits(value));
+		return ptr;
+	}
+
+	public int write_(int ptr, float value) {
+		ptr = write_(memory, ptr, Float.floatToIntBits(value));
+		return ptr;
+	}
+
+	public int write_(int ptr, float[] floats) {
+		for (float value : floats)
+			ptr = write_(memory, ptr, Float.floatToIntBits(value));
+		return ptr;
+	}
+
+	public int write_(int ptr, int value) {
+		ptr = write_(memory, ptr, (byte) ((value >> 24) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 16) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 8) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 0) & 0xff));
+		return ptr;
+	}
+
+	public int write_(int ptr, int[] ints) {
+		for (int value : ints)
+			ptr = write_(memory, ptr, value);
+		return ptr;
+	}
+
+	public int write_(int ptr, long value) {
+		ptr = write_(memory, ptr, (byte) ((value >> 56) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 48) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 40) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 32) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 24) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 16) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 8) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 0) & 0xff));
+		return ptr;
+	}
+
+	public int write_(int ptr, long[] longs) {
+		for (long value : longs)
+			ptr = write_(memory, ptr, value);
+		return ptr;
+	}
+
+	public int write_(int ptr, short value) {
+		ptr = write_(memory, ptr, (byte) ((value >> 8) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 0) & 0xff));
+		return ptr;
+	}
+
+	public int write_(int ptr, short[] shorts) {
+		for (short value : shorts)
+			ptr = write_(memory, ptr, value);
+		return ptr;
+	}
+
+	public static int write_(Memory memory, int ptr, boolean value) {
+		ptr = write_(memory, ptr, (byte) ((value) ? 0x01 : 0x00));
+		return ptr;
+	}
+
+	public static int write_(Memory memory, int ptr, boolean[] booleans) {
+		for (boolean value : booleans)
+			ptr = write_(memory, ptr, value);
+		return ptr;
+	}
 
 	public static int write_(Memory memory, int ptr, byte value) {
 		memory.alloc_f[ptr] = true;
@@ -38,18 +159,6 @@ public class MemoryWriter {
 		return ptr;
 	}
 
-	public static int write_(Memory memory, int ptr, short value) {
-		ptr = write_(memory, ptr, (byte) ((value >> 8) & 0xff));
-		ptr = write_(memory, ptr, (byte) ((value >> 0) & 0xff));
-		return ptr;
-	}
-
-	public static int write_(Memory memory, int ptr, short[] shorts) {
-		for (short value : shorts)
-			ptr = write_(memory, ptr, value);
-		return ptr;
-	}
-	
 	public static int write_(Memory memory, int ptr, char value) {
 		ptr = write_(memory, ptr, (byte) ((value >> 8) & 0xff));
 		ptr = write_(memory, ptr, (byte) ((value >> 0) & 0xff));
@@ -59,6 +168,28 @@ public class MemoryWriter {
 	public static int write_(Memory memory, int ptr, char[] chars) {
 		for (char value : chars)
 			ptr = write_(memory, ptr, value);
+		return ptr;
+	}
+
+	public static int write_(Memory memory, int ptr, double value) {
+		ptr = write_(memory, ptr, Double.doubleToLongBits(value));
+		return ptr;
+	}
+
+	public static int write_(Memory memory, int ptr, double[] doubles) {
+		for (double value : doubles)
+			ptr = write_(memory, ptr, Double.doubleToLongBits(value));
+		return ptr;
+	}
+
+	public static int write_(Memory memory, int ptr, float value) {
+		ptr = write_(memory, ptr, Float.floatToIntBits(value));
+		return ptr;
+	}
+
+	public static int write_(Memory memory, int ptr, float[] floats) {
+		for (float value : floats)
+			ptr = write_(memory, ptr, Float.floatToIntBits(value));
 		return ptr;
 	}
 
@@ -94,35 +225,14 @@ public class MemoryWriter {
 		return ptr;
 	}
 
-	public static int write_(Memory memory, int ptr, float value) {
-		ptr = write_(memory, ptr, Float.floatToIntBits(value));
+	public static int write_(Memory memory, int ptr, short value) {
+		ptr = write_(memory, ptr, (byte) ((value >> 8) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 0) & 0xff));
 		return ptr;
 	}
 
-	public static int write_(Memory memory, int ptr, float[] floats) {
-		for (float value : floats)
-			ptr = write_(memory, ptr, Float.floatToIntBits(value));
-		return ptr;
-	}
-
-	public static int write_(Memory memory, int ptr, double value) {
-		ptr = write_(memory, ptr, Double.doubleToLongBits(value));
-		return ptr;
-	}
-
-	public static int write_(Memory memory, int ptr, double[] doubles) {
-		for (double value : doubles)
-			ptr = write_(memory, ptr, Double.doubleToLongBits(value));
-		return ptr;
-	}
-
-	public static int write_(Memory memory, int ptr, boolean value) {
-		ptr = write_(memory, ptr, (byte) ((value) ? 0x01 : 0x00));
-		return ptr;
-	}
-
-	public static int write_(Memory memory, int ptr, boolean[] booleans) {
-		for (boolean value : booleans)
+	public static int write_(Memory memory, int ptr, short[] shorts) {
+		for (short value : shorts)
 			ptr = write_(memory, ptr, value);
 		return ptr;
 	}
