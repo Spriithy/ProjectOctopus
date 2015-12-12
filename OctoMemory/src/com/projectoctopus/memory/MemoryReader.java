f4a7b70cf7490e4ced3e4c2896a389a888f03b4f
Spriithy <theo.dano@gmail.com>
2015-12-12 23:56:40 +0100

    Added write and read methods for Strings
    Organized methods position in classes

--- OctoMemory/src/com/projectoctopus/memory/MemoryReader.java
+++ OctoMemory/src/com/projectoctopus/memory/MemoryReader.java
@@ -26,8 +26,9 @@ package com.projectoctopus.memory;
  */
 public class MemoryReader {
 
-	private Memory	memory;
-	private boolean	linked	= false;
+	private Memory memory;
+
+	private boolean linked = false;
 
 	public void link(Memory memory) {
 		assert(!linked);
@@ -35,48 +36,60 @@ public class MemoryReader {
 		this.memory = memory;
 	}
 
-	public byte read_byte(int ptr) {
-		return read_byte(memory.data, ptr);
+	public static boolean read_bool(byte[] source, int ptr) {
+		assert(source[ptr] == 0 || source[ptr] == 1);
+		return source[ptr] != 0;
 	}
 
-	public short read_short(int ptr) {
-		return read_short(memory.data, ptr);
-	}
-
-	public char read_char(int ptr) {
-		return read_char(memory.data, ptr);
-	}
-
-	public int read_int(int ptr) {
-		return read_int(memory.data, ptr);
-	}
-
-	public long read_long(int ptr) {
-		return read_long(memory.data, ptr);
-	}
-
-	public float read_float(int ptr) {
-		return read_float(memory.data, ptr);
-	}
-
-	public double read_double(int ptr) {
-		return read_double(memory.data, ptr);
-	}
-
-	public boolean read_bool(int ptr) {
-		return read_bool(memory.data, ptr);
+	public static boolean[] read_bool_array(byte[] source, int ptr, int size) {
+		boolean[] array = new boolean[size];
+		for (int p = ptr; p < ptr + size; p++)
+			array[p - ptr] = read_bool(source, p);
+		return array;
 	}
 
 	public static byte read_byte(byte[] source, int ptr) {
 		return (byte) (0xff & source[ptr]);
 	}
 
-	public static short read_short(byte[] source, int ptr) {
-		return (short) ((0xff & source[ptr++]) << 8 | (0xff & source[ptr]));
+	public static byte[] read_byte_array(byte[] source, int ptr, int size) {
+		byte[] array = new byte[size];
+		for (int p = ptr; p < ptr + size; p++)
+			array[p - ptr] = read_byte(source, p);
+		return array;
 	}
 
 	public static char read_char(byte[] source, int ptr) {
 		return (char) ((0xff & source[ptr++]) << 8 | (0xff & source[ptr]));
+	}
+
+	public static char[] read_char_array(byte[] source, int ptr, int size) {
+		char[] array = new char[size];
+		for (int p = ptr; p < ptr + size; p++)
+			array[p - ptr] = read_char(source, p);
+		return array;
+	}
+
+	public static double read_double(byte[] source, int ptr) {
+		return Double.longBitsToDouble(read_long(source, ptr));
+	}
+
+	public static double[] read_double_array(byte[] source, int ptr, int size) {
+		double[] array = new double[size];
+		for (int p = ptr; p < ptr + size; p++)
+			array[p - ptr] = read_double(source, p);
+		return array;
+	}
+
+	public static float read_float(byte[] source, int ptr) {
+		return Float.intBitsToFloat(read_int(source, ptr));
+	}
+
+	public static float[] read_float_array(byte[] source, int ptr, int size) {
+		float[] array = new float[size];
+		for (int p = ptr; p < ptr + size; p++)
+			array[p - ptr] = read_float(source, p);
+		return array;
 	}
 
 	public static int read_int(byte[] source, int ptr) {
@@ -86,6 +99,13 @@ public class MemoryReader {
 				(int) (0xff & source[ptr++]) << 0);
 	}
 
+	public static int[] read_int_array(byte[] source, int ptr, int size) {
+		int[] array = new int[size];
+		for (int p = ptr; p < ptr + size; p++)
+			array[p - ptr] = read_int(source, p);
+		return array;
+	}
+
 	public static long read_long(byte[] source, int ptr) {
 		return (long) ((long) (0xff & source[ptr++]) << 56 | (long) (0xff & source[ptr++]) << 48 |
 				(long) (0xff & source[ptr++]) << 40 | (long) (0xff & source[ptr++]) << 32 |
@@ -93,16 +113,110 @@ public class MemoryReader {
 				(long) (0xff & source[ptr++]) << 8 | (long) (0xff & source[ptr++]) << 0);
 	}
 
-	public static float read_float(byte[] source, int ptr) {
-		return Float.intBitsToFloat(read_int(source, ptr));
+	public static long[] read_long_array(byte[] source, int ptr, int size) {
+		long[] array = new long[size];
+		for (int p = ptr; p < ptr + size; p++)
+			array[p - ptr] = read_long(source, p);
+		return array;
 	}
 
-	public static double read_double(byte[] source, int ptr) {
-		return Double.longBitsToDouble(read_long(source, ptr));
+	public static short read_short(byte[] source, int ptr) {
+		return (short) ((0xff & source[ptr++]) << 8 | (0xff & source[ptr]));
 	}
 
-	public static boolean read_bool(byte[] source, int ptr) {
-		assert(source[ptr] == 0 || source[ptr] == 1);
-		return source[ptr] != 0;
+	public static short[] read_short_array(byte[] source, int ptr, int size) {
+		short[] array = new short[size];
+		for (int p = ptr; p < ptr + size; p++)
+			array[p - ptr] = read_short(source, p);
+		return array;
 	}
+
+	public static String read_string(byte[] source, int ptr) {
+		short size = read_short(source, ptr++);
+		return new String(read_byte_array(source, ++ptr, size));
+	}
+
+	public static String[] read_string_array(byte[] source, int ptr, int size) {
+		String[] array = new String[size];
+		for (int p = ptr; p < ptr + size; p++)
+			array[p - ptr] = read_string(source, p);
+		return array;
+	}
+
+	public boolean read_bool(int ptr) {
+		return read_bool(memory.data, ptr);
+	}
+
+	public boolean[] read_bool_array(int ptr, int size) {
+		return read_bool_array(memory.data, ptr, size);
+	}
+
+	public byte read_byte(int ptr) {
+		return read_byte(memory.data, ptr);
+	}
+
+	public byte[] read_byte_array(int ptr, int size) {
+		return read_byte_array(memory.data, ptr, size);
+	}
+
+	public char read_char(int ptr) {
+		return read_char(memory.data, ptr);
+	}
+
+	public char[] read_char_array(int ptr, int size) {
+		return read_char_array(memory.data, ptr, size);
+	}
+
+	public double read_double(int ptr) {
+		return read_double(memory.data, ptr);
+	}
+
+	public double[] read_double_array(int ptr, int size) {
+		return read_double_array(memory.data, ptr, size);
+	}
+
+	public float read_float(int ptr) {
+		return read_float(memory.data, ptr);
+	}
+
+	public float[] read_float_array(int ptr, int size) {
+		return read_float_array(memory.data, ptr, size);
+	}
+
+	public int read_int(int ptr) {
+		return read_int(memory.data, ptr);
+	}
+
+	public int[] read_int_array(int ptr, int size) {
+		return read_int_array(memory.data, ptr, size);
+	}
+
+	public long read_long(int ptr) {
+		return read_long(memory.data, ptr);
+	}
+
+	public long[] read_long_array(int ptr, int size) {
+		return read_long_array(memory.data, ptr, size);
+	}
+
+	public short read_short(int ptr) {
+		return read_short(memory.data, ptr);
+	}
+
+	public short[] read_short_array(int ptr, int size) {
+		return read_short_array(memory.data, ptr, size);
+	}
+
+	public String read_string(int ptr) {
+		short size = read_short(memory.data, ptr++);
+		return new String(read_byte_array(memory.data, ++ptr, size));
+	}
+
+	public String[] read_string_array(int ptr, int size) {
+		String[] array = new String[size];
+		for (int p = ptr; p < ptr + size; p++)
+			array[p - ptr] = read_string(memory.data, p);
+		return array;
+	}
+
 }
--- OctoMemory/src/com/projectoctopus/memory/MemoryWriter.java
+++ OctoMemory/src/com/projectoctopus/memory/MemoryWriter.java
@@ -35,107 +35,6 @@ public class MemoryWriter {
 		this.memory = memory;
 	}
 
-	public int write_(int ptr, boolean value) {
-		ptr = write_(memory, ptr, (byte) ((value) ? 0x01 : 0x00));
-		return ptr;
-	}
-
-	public int write_(int ptr, boolean[] booleans) {
-		for (boolean value : booleans)
-			ptr = write_(memory, ptr, value);
-		return ptr;
-	}
-
-	public int write_(int ptr, byte value) {
-		memory.alloc_f[ptr] = true;
-		memory.data[ptr++] = value;
-		return ptr;
-	}
-
-	public int write_(int ptr, byte[] bytes) {
-		for (byte value : bytes)
-			ptr = write_(memory, ptr, value);
-		return ptr;
-	}
-
-	public int write_(int ptr, char value) {
-		ptr = write_(memory, ptr, (byte) ((value >> 8) & 0xff));
-		ptr = write_(memory, ptr, (byte) ((value >> 0) & 0xff));
-		return ptr;
-	}
-
-	public int write_(int ptr, char[] chars) {
-		for (char value : chars)
-			ptr = write_(memory, ptr, value);
-		return ptr;
-	}
-
-	public int write_(int ptr, double value) {
-		ptr = write_(memory, ptr, Double.doubleToLongBits(value));
-		return ptr;
-	}
-
-	public int write_(int ptr, double[] doubles) {
-		for (double value : doubles)
-			ptr = write_(memory, ptr, Double.doubleToLongBits(value));
-		return ptr;
-	}
-
-	public int write_(int ptr, float value) {
-		ptr = write_(memory, ptr, Float.floatToIntBits(value));
-		return ptr;
-	}
-
-	public int write_(int ptr, float[] floats) {
-		for (float value : floats)
-			ptr = write_(memory, ptr, Float.floatToIntBits(value));
-		return ptr;
-	}
-
-	public int write_(int ptr, int value) {
-		ptr = write_(memory, ptr, (byte) ((value >> 24) & 0xff));
-		ptr = write_(memory, ptr, (byte) ((value >> 16) & 0xff));
-		ptr = write_(memory, ptr, (byte) ((value >> 8) & 0xff));
-		ptr = write_(memory, ptr, (byte) ((value >> 0) & 0xff));
-		return ptr;
-	}
-
-	public int write_(int ptr, int[] ints) {
-		for (int value : ints)
-			ptr = write_(memory, ptr, value);
-		return ptr;
-	}
-
-	public int write_(int ptr, long value) {
-		ptr = write_(memory, ptr, (byte) ((value >> 56) & 0xff));
-		ptr = write_(memory, ptr, (byte) ((value >> 48) & 0xff));
-		ptr = write_(memory, ptr, (byte) ((value >> 40) & 0xff));
-		ptr = write_(memory, ptr, (byte) ((value >> 32) & 0xff));
-		ptr = write_(memory, ptr, (byte) ((value >> 24) & 0xff));
-		ptr = write_(memory, ptr, (byte) ((value >> 16) & 0xff));
-		ptr = write_(memory, ptr, (byte) ((value >> 8) & 0xff));
-		ptr = write_(memory, ptr, (byte) ((value >> 0) & 0xff));
-		return ptr;
-	}
-
-	public int write_(int ptr, long[] longs) {
-		for (long value : longs)
-			ptr = write_(memory, ptr, value);
-		return ptr;
-	}
-
-	public int write_(int ptr, short value) {
-		ptr = write_(memory, ptr, (byte) ((value >> 8) & 0xff));
-		ptr = write_(memory, ptr, (byte) ((value >> 0) & 0xff));
-		return ptr;
-	}
-
-	public int write_(int ptr, short[] shorts) {
-		for (short value : shorts)
-			ptr = write_(memory, ptr, value);
-		return ptr;
-	}
-
 	public static int write_(Memory memory, int ptr, boolean value) {
 		ptr = write_(memory, ptr, (byte) ((value) ? 0x01 : 0x00));
 		return ptr;
@@ -232,6 +131,115 @@ public class MemoryWriter {
 	}
 
 	public static int write_(Memory memory, int ptr, short[] shorts) {
+		for (short value : shorts)
+			ptr = write_(memory, ptr, value);
+		return ptr;
+	}
+
+	public static int write_(Memory memory, int ptr, String str) {
+		ptr = write_(memory, ptr, (short) str.length());
+		for (byte value : str.getBytes())
+			ptr = write_(memory, ptr, value);
+		return ptr;
+
+	}
+
+	public int write_(int ptr, boolean value) {
+		ptr = write_(memory, ptr, (byte) ((value) ? 0x01 : 0x00));
+		return ptr;
+	}
+
+	public int write_(int ptr, boolean[] booleans) {
+		for (boolean value : booleans)
+			ptr = write_(memory, ptr, value);
+		return ptr;
+	}
+
+	public int write_(int ptr, byte value) {
+		memory.alloc_f[ptr] = true;
+		memory.data[ptr++] = value;
+		return ptr;
+	}
+
+	public int write_(int ptr, byte[] bytes) {
+		for (byte value : bytes)
+			ptr = write_(memory, ptr, value);
+		return ptr;
+	}
+
+	public int write_(int ptr, char value) {
+		ptr = write_(memory, ptr, (byte) ((value >> 8) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 0) & 0xff));
+		return ptr;
+	}
+
+	public int write_(int ptr, char[] chars) {
+		for (char value : chars)
+			ptr = write_(memory, ptr, value);
+		return ptr;
+	}
+
+	public int write_(int ptr, double value) {
+		ptr = write_(memory, ptr, Double.doubleToLongBits(value));
+		return ptr;
+	}
+
+	public int write_(int ptr, double[] doubles) {
+		for (double value : doubles)
+			ptr = write_(memory, ptr, Double.doubleToLongBits(value));
+		return ptr;
+	}
+
+	public int write_(int ptr, float value) {
+		ptr = write_(memory, ptr, Float.floatToIntBits(value));
+		return ptr;
+	}
+
+	public int write_(int ptr, float[] floats) {
+		for (float value : floats)
+			ptr = write_(memory, ptr, Float.floatToIntBits(value));
+		return ptr;
+	}
+
+	public int write_(int ptr, int value) {
+		ptr = write_(memory, ptr, (byte) ((value >> 24) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 16) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 8) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 0) & 0xff));
+		return ptr;
+	}
+
+	public int write_(int ptr, int[] ints) {
+		for (int value : ints)
+			ptr = write_(memory, ptr, value);
+		return ptr;
+	}
+
+	public int write_(int ptr, long value) {
+		ptr = write_(memory, ptr, (byte) ((value >> 56) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 48) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 40) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 32) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 24) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 16) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 8) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 0) & 0xff));
+		return ptr;
+	}
+
+	public int write_(int ptr, long[] longs) {
+		for (long value : longs)
+			ptr = write_(memory, ptr, value);
+		return ptr;
+	}
+
+	public int write_(int ptr, short value) {
+		ptr = write_(memory, ptr, (byte) ((value >> 8) & 0xff));
+		ptr = write_(memory, ptr, (byte) ((value >> 0) & 0xff));
+		return ptr;
+	}
+
+	public int write_(int ptr, short[] shorts) {
 		for (short value : shorts)
 			ptr = write_(memory, ptr, value);
 		return ptr;
